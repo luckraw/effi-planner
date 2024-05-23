c52ee3a534cc906e00d6943bcd7b3e3b7693cd51
Lucas <krawdev@gmail.com>
2024-05-23 17:27:18 -0300

    project created

---
+++ src/main/java/com/luckraw/effi_planner/EffiPlannerApplication.java
@@ -0,0 +1,13 @@
+package com.luckraw.effi_planner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EffiPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EffiPlannerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luckraw/effi_planner/EffiPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luckraw.effi_planner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EffiPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
