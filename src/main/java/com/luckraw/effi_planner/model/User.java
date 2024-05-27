9b122721c07749317272fda78e57a2c3370edf3d
Lucas <krawdev@gmail.com>
2024-05-27 10:53:40 -0300

    feat: entities created

---
+++ src/main/java/com/luckraw/effi_planner/model/User.java
@@ -0,0 +1,14 @@
+package com.luckraw.effi_planner.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+}
